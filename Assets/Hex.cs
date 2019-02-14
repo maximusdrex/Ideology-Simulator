3ed9a3b1764dc34a352d87987ee699022f2c6aee
Max Schaefer <max@theschaefers.com>
2019-02-14 09:36:49 -0800

    merge conflicts

---
+++ Assets/Hex.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class defines and does the math to determine the world location for 
+/// a given hex tile.
+/// </summary>
+public class Hex
+{
+    static readonly float WIDTH_MOD = Mathf.Sqrt(3)/2;
+    //c + r + s = 0 (must)
+    //cubic collumn
+    public readonly int C;
+    //cubic row
+    public readonly int R;
+    //cubic z
+    public readonly int S;
+
+    public Hex (int c, int r)
+    {
+        this.C = c;
+        this.R = r;
+        this.S = -(c + r);
+    }  
+
+    /// <summary>
+    /// Calculates the position in the world of a given hex
+    /// </summary>
+    /// <returns>The position.</returns>
+    public Vector3 GetPosition()
+    {
+        float radius = 1f;
+        float diameter = radius * 2;
+        float width = WIDTH_MOD * diameter;
+        float horizontal = width;
+        float vertical = diameter * .75f;
+        return new Vector3(horizontal*(this.C + this.R/2f), 0, vertical * this.R);
+    }
+}
---
+++ Assets/HexMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        createMap();
+    }
+
+    public Material[] HexTerrains;
+
+    public GameObject HexPrefab;
+
+    readonly int numRows = 20;
+    readonly int numCollummns = 40;
+
+    public void createMap()
+    {
+        for(int col = 0; col < numCollummns; col++)
+        {
+            for(int row = 0; row< numRows; row++)
+            {
+                Hex h = new Hex(col, row);
+                //Add Hex
+                GameObject hexObject = (GameObject) Instantiate(HexPrefab, h.GetPosition(), Quaternion.identity, this.transform);
+
+                MeshRenderer mr = hexObject.GetComponentInChildren<MeshRenderer>();
+                mr.material = HexTerrains[Random.Range(0, HexTerrains.Length)];
+            }
+        }
+        //While does cut down on number of batches to render, it produces a gross ripple
+        //And doesn't let you move camer
+        //StaticBatchingUtility.Combine(this.gameObject);
+    }
+}
