9cf199f179aa00843b872e1f629ced4ebff2b36a
Cayden Franklin <caydenf19@lakesideschool.org>
2019-03-05 10:23:16 -0800

    camera movement

--- Assets/CameraMotion.cs
+++ Assets/CameraMotion.cs
@@ -7,6 +7,7 @@ public class CameraMotion : MonoBehaviour
     public Vector3 lastPosition;
     public bool hasMoved;
     public int magnitude;
+    public float panSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -14,11 +15,13 @@ public class CameraMotion : MonoBehaviour
         lastPosition = this.transform.position;
         hasMoved = true;
         magnitude = 10;
+        panSpeed = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = this.transform.position;
         if(Mathf.Abs(this.transform.position.x - lastPosition.x) > 10)
         {
             lastPosition = this.transform.position;
@@ -28,5 +31,30 @@ public class CameraMotion : MonoBehaviour
         {
             hasMoved = false;
         }
+        if(Input.GetKey("w"))
+        {
+           pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("q"))
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("e"))
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+        transform.position = pos;
     }
 }
