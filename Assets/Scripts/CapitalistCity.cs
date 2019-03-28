039934085b92799558864addfe175edf74b0848a
Max Schaefer <max@theschaefers.com>
2019-03-28 10:39:43 -0700

    premerge

--- Assets/Scripts/CapitalistCity.cs
+++ Assets/Scripts/CapitalistCity.cs
@@ -47,7 +47,7 @@ public class CapitalistCity : City
             foreach(Building b in stores)
             {
                 Store s = (Store)b;
-                if(c.wealth >=  s.getPrice())
+                //if(c.wealth >=  s.getPrice())
             }
         }
     }
