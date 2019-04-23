d54635c08e4f79fb58f20d68dc0c1b545ab3014e
Cayden Franklin <caydenf19@lakesideschool.org>
2019-04-23 10:15:48 -0700

    worker creation

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -52,7 +52,8 @@ public class PlayerManager : MonoBehaviour
                     if(hexList.Count == 1)
                     {
                         setGUI(hexList[0].GetUI());
-                    } else
+                    } 
+                    else
                     {
                         setGUI(defaultGUI);
                     }
